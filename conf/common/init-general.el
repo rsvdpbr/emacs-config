3e9b5d5e0f354c5cb1fee1c53358b88e52ced777
Ryo Nishikawa <ryo.nishikawa.re@gmail.com>
2013-01-27 18:43:20 +0900

    選択範囲の文字数・行数の情報を表示

--- conf/common/init-general.el
+++ conf/common/init-general.el
@@ -63,6 +63,16 @@
 (line-number-mode t)
 (column-number-mode t)
 
+;; 選択範囲の情報表示
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "[%3d:%4d]"
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+    ""))
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+
 ;; カーソルの位置を保存する
 (require 'saveplace)
 
