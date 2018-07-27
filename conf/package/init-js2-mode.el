e27cd6b1bc77cce52a64bc9abff43ec64512bc73
Ryo Nishikawa <ryo.nishikawa.re@gmail.com>
2018-07-27 11:41:45 -0700

    Update json-mode config

--- conf/package/init-js2-mode.el
+++ conf/package/init-js2-mode.el
@@ -10,3 +10,8 @@
 (add-hook 'js2-mode-hook 'prettier-js-mode)
 
 (setq js-indent-level 2)
+
+(add-hook 'json-mode-hook
+          (lambda ()
+            (make-local-variable 'js-indent-level)
+            (setq js-indent-level 2)))
