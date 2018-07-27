62f6aae16ba3f99ea3fc4df9a619ec4aac1308df
Ryo Nishikawa <ryo.nishikawa.re@gmail.com>
2018-07-27 11:41:33 -0700

    Add next/previous bindings for company-mode

--- conf/package/init-company.el
+++ conf/package/init-company.el
@@ -6,6 +6,8 @@
 (define-key company-active-map (kbd "C-h") nil)
 (define-key company-active-map (kbd "<tab>") 'company-complete-common-or-cycle)
 (define-key company-active-map (kbd "M-d") 'company-show-doc-buffer)
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
 
 (setq company-minimum-prefix-length 1)
 (setq company-selection-wrap-around t)
